d0060c1d7ed79ce4d201dc4896bc19265723fb37
David Von Lehman <david.vonlehman@gmail.com>
2013-09-13 10:36:18 -0700

    Fixing relative path

--- pull_master.sh
+++ pull_master.sh
@@ -1,3 +1,5 @@
-sudo curl -x webproxy.nordstrom.net:80 -v -o tdsql.zip https://codeload.github.com/Nordstrom/tdsql/zip/master
-unzip -o -j -d . tdsql.zip
+curl -x webproxy.nordstrom.net:80 -v -o tdsql.zip https://codeload.github.com/Nordstrom/tdsql/zip/master
+unzip -o tdsql.zip
+mv tdsql-master/* .
+rm -Rf tdsql-master
 rm tdsql.zip
