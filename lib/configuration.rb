76fe9d63a18122bac4fdf063e74edbb8a99a57c9
David Von Lehman <david.vonlehman@gmail.com>
2014-10-27 11:15:06 -0700

    Fixing strip error in the ddl command

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -92,14 +92,14 @@ class Configuration
 	  end
 	end
 
-	def determine_ddl_cmd()			
+	def determine_ddl_cmd()
 		if not blank?(@config_settings[:ddlfile])
 			raise ConfigError, "File #{@config_settings[:ddlfile]} does not exist" unless File.exist?(@config_settings[:ddlfile])
 
-			File.open(@config_settings[:ddl], 'rb') do |file|
+			File.open(@config_settings[:ddlfile], 'rb') do |file|
 				return file.read
 			end
-		elsif not blank?(@config_settings[:ddl])
+		elsif not @config_settings[:ddl].nil? and not blank?(@config_settings[:ddl])
 			@config_settings[:ddl].strip().delete('"')
 		else
 			return nil
